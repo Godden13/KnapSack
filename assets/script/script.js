6de8d4630353021a2a93f4735007087e165a3f73
Chriss Godden <godden@Chrisss-MacBook-Pro.local>
2022-12-16 12:34:05 +0100

    commit new changes

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,45 +3,58 @@ let itemList = document.getElementById('items')
 let doneButtonElement = document.getElementById('done');
 let display = document.getElementById('showscreen');
 let displayWeight = document.getElementById('calc');
-let reset = document.getElementById('reset').reset();
-let calculateWeight = 0;
 let count = 0;
 
-doneButtonElement.addEventListener('click', () =>{
-    if (maxWeight.value == ""){
+let knapsack = {
+    capacity: 0,
+    weight: 0,
+    value: 0,
+    items: []
+}
+
+console.log(knapsack)
+
+doneButtonElement.addEventListener('click', () => {
+    if (maxWeight.value == "") {
         maxWeight.style.border = "2px solid red"
 
-        maxWeight.placeholder = 'please enter a value';
+        maxWeight.placeholder = '⚠️ please enter a value';
     } else {
+        knapsack.capacity = maxWeight.value
+        maxWeight.disabled = true
+        console.log(knapsack.capacity)
+
         itemList.addEventListener('change', () => {
-            if(calculateWeight <= maxWeight.value){
-                display.innerHTML += itemList.value + "<hr>" + "<br> ";
-                for(let i = 0; i < arr.length; i++) {
-                    if(itemList.value == arr[i].name) {
-                        calculateWeight += arr[i].weight;
-                        displayWeight.innerHTML = "<p>The sum of the Weight</p>" + calculateWeight;
-                }
-                }
-            } if (calculateWeight + arr[i] > maxWeight) {
-                if(itemList[i].value == arr[i].name){
-                    itemlist[i].disabled = true;
-                    if (itemlist[i].disabled == true) {
-                        count++;
+            for (let i = 0; i < arr.length; i++) {
+                if (itemList.value == arr[i].name) {
+                    if (knapsack.weight + arr[i].weight <= knapsack.capacity) {
+                        let itemSelected = itemList.selectedIndex
+                        itemSelected.disabled = true;
+                        knapsack.items.push(arr[i])
+                        knapsack.weight += arr[i].weight
+                        knapsack.value += arr[i].value
+                        console.log(knapsack)
+                        display.innerHTML = knapsack.weight + ' ' + 'is the weight' + ', ' + knapsack.value + ' ' + 'is the value'
+                    } if (knapsack.weight + arr[i].weight > knapsack.capacity) {
+                        alert('The Knapsack will be full by adding this item')
                     }
+                } if (knapsack.weight == knapsack.capacity) {
+                    document.querySelector("#showscreen").style.background = "red";
+                    document.querySelector("#showscreen").style.color = "#fff";
                 }
-               
+                console.log(arr[i], itemList.value)
             }
         })
     }
 })
 
 var arr = [
-    {name: "item 1", weight: 4, value: 100},
-    {name: "item 2", weight: 9, value: 200},
-    {name: "item 3", weight: 3, value: 400},
-    {name: "item 4", weight: 6, value: 200},
-    {name: "item 5", weight: 8, value: 300},
-    {name: "item 6", weight: 7, value: 800},
-    {name: "item 7", weight: 8, value: 600},
-    {name: "item 8", weight: 5, value: 700}
+    { name: "item 1", weight: 4, value: 100 },
+    { name: "item 2", weight: 1, value: 200 },
+    { name: "item 3", weight: 3, value: 400 },
+    { name: "item 4", weight: 6, value: 200 },
+    { name: "item 5", weight: 8, value: 300 },
+    { name: "item 6", weight: 7, value: 800 },
+    { name: "item 7", weight: 8, value: 600 },
+    { name: "item 8", weight: 5, value: 700 }
 ]
